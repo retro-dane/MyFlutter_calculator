9c6090d60603d80a6e62cbbde45a905d757dacce
retro-dane <43159871+retro-dane@users.noreply.github.com>
2022-02-16 20:50:47 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'HomePage.dart';
+
+void main() => runApp(const MyCalculator());
+
+class MyCalculator extends StatelessWidget {
+  const MyCalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Calculator'),
+    );
+  }
+}
+
